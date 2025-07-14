62f9411552c469bb13eed367a0399cac92828305
Krzysztof Michalczyk <kpm@krzysckh.org>
2025-07-14 19:17:55 +0000

    add datetime

--- ddatetime.c
+++ ddatetime.c
@@ -1,5 +1,7 @@
 #include "uxn9.h"
 
+#define Nsec 1000000000
+
 #define DATETIME_YEAR   0xc0
 #define DATETIME_MONTH  0xc2
 #define DATETIME_DAY    0xc3
@@ -26,16 +28,35 @@ static Tm tm;
 static void
 update_tm(void)
 {
-  tmtimens(&tm, 0, 0, tz);
+  vlong now = nsec(), s;
+  s = now/Nsec;
+  now = now%Nsec;
+  if (tmtimens(&tm, s, now, tz) == nil)
+    sysfatal("tmtimens");
 }
 
-DEFDATIME(second, sec);
 DEFDATIME(year, year + 1900);
+DEFDATIME(month, mon);
+DEFDATIME(day, mday);
+DEFDATIME(hour, hour);
+DEFDATIME(minute, min);
+DEFDATIME(second, sec);
+DEFDATIME(dotw, wday);
+DEFDATIME(doty, yday);
 
 void
 init_datetime_device(Uxn *uxn)
 {
   tz = tzload("local");
+  if (tz == nil)
+    sysfatal("tzload");
+
+  uxn->devices[DATETIME_YEAR  ] = datetime_year;
+  uxn->devices[DATETIME_MONTH ] = datetime_month;
+  uxn->devices[DATETIME_DAY   ] = datetime_day;
+  uxn->devices[DATETIME_HOUR  ] = datetime_hour;
+  uxn->devices[DATETIME_MINUTE] = datetime_minute;
   uxn->devices[DATETIME_SECOND] = datetime_second;
-  uxn->devices[DATETIME_YEAR] = datetime_year;
+  uxn->devices[DATETIME_DOTW  ] = datetime_dotw;
+  uxn->devices[DATETIME_DOTY  ] = datetime_doty;
 }
